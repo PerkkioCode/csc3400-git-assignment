ea9e2a9fd5a37f59b4af3ee1f21209804cfad458
Connor Perkkio <cperkkio@mocs.flsouthern.edu>
2026-01-16 19:40:42 -0500

    Add advanced mathematical operations

--- calculator.py
+++ calculator.py
@@ -14,12 +14,24 @@ def multiply(a, b):
 
 def divide(a, b):
     if b == 0:
-        return 
+        return "Error: cannot divide by zero"
     return a / b
+
+
+def power(a,b):
+    return a ** b 
+
+
+def square_root(a):
+    if a < 0:
+        return "Error: cannot take square root of a negative number"
+    return a ** 0.5
 
 
 if __name__ == "__main__":
     print("Add:", add(5, 3))
     print("Subtract:", subtract(5, 3))
     print("Multiply:", multiply(5, 3))
-    print("Divide:", divide(5, 3))
+    print("Divide:", divide(5, 0))
+    print("Power:", power(2,3))	
+    print("Square Root:", square_root(-9))
